4339bc5ef99d0b408edf393541acb6234b80923d
clement bacle <clement@officinasante.fr>
2018-03-18 09:05:47 +0100

    has_many adding

--- app/models/mood_check.rb
+++ app/models/mood_check.rb
@@ -1,3 +1,4 @@
 class MoodCheck < ApplicationRecord
   belongs_to :profil
+  has_many :actions
 end
--- app/models/profil.rb
+++ app/models/profil.rb
@@ -1,3 +1,4 @@
 class Profil < ApplicationRecord
   belongs_to :user
+  has_many :mood_checks
 end
